219fbfbd8ed5756927a008b0b40cd769ab2a985f
André Thomas Monteiro <andre.thomas.monteiro@gmail.com>
2023-06-20 10:18:42 +0200

    read_bts() is now handling buttons presses

--- example_project/example_project.ino
+++ example_project/example_project.ino
@@ -5,25 +5,25 @@
 
 // =====================================================================================
 // --- Hardware Mapping ---
-#define db6 6         //LCD
-#define db7 7         //LCD
-#define db5 5         //LCD
-#define db4 4         //LCD
-#define db3 11        //LCD
-#define db2 10        //LCD
-#define db1 9         //LCD
-#define db0 8         //LCD
-#define rs 13         //LCD
-#define en 12         //LCD
+#define db6 6          //LCD
+#define db7 7          //LCD
+#define db5 5          //LCD
+#define db4 4          //LCD
+#define db3 11         //LCD
+#define db2 10         //LCD
+#define db1 9          //LCD
+#define db0 8          //LCD
+#define rs 13          //LCD
+#define en 12          //LCD
 
 #define menu_bt 19     //Push button  [Analogic 5]
-#define enter_bt  18     //Push button  [Analogic 4]
-#define up_bt   17     //Push button  [Analogic 3]
+#define enter_bt 18    //Push button  [Analogic 4]
+#define up_bt 17       //Push button  [Analogic 3]
 #define down_bt 16     //Push button  [Analogic 2]
 
-#define sensor_temp 0 //Temperature sensor [Analogic 0]
+#define sensor_temp 0  // Temperature sensor [Analogic 0]
 
-#define led 2        //Led
+#define led 2          // Led
 
 // =====================================================================================
 // --- Project Constants ---
@@ -33,32 +33,32 @@ const int debounce_time = 30;  // time in milliseconds
 
 // =====================================================================================
 // --- Function Prototypes ---
-void  read_bts();
-bool  read_bt_enter();
-bool  read_bt_menu();
-bool  read_bt_up();
-bool  read_bt_down();
+void read_bts();            // Read if any of the buttons got pressed
+bool read_bt_enter();       // Read if enter button got pressed return 1(True) or 0(False)
+bool read_bt_menu();        // Read in menu button got pressed return 1(True) or 0(False)
+bool read_bt_down();        // Read in down button got pressed return 1(True) or 0(False)
+bool read_bt_up();          // Read in up button got pressed return 1(True) or 0(False)
 
-void  menu_select();
-float read_temperature();
+void menu_select();         // Open up the submenu for the selected option
+float read_temperature();   // Reads the temperature from the heat sensor and return the temperature in ºC
 
 
 // =====================================================================================
 // --- Global Variables ---
-int menu_number = 1;           // number of current menu 
-bool sub_menu = 0;             // inside menu_select()
-bool update_menu_flag = 1;     // flag for updating the menu
+int menu_number = 1;        // number of current menu
+bool sub_menu = 0;          // inside menu_select()
+bool update_menu_flag = 1;  // flag for updating the menu
 
 // =====================================================================================
 // --- Inicial config ---
-LiquidCrystal lcd(rs, en, db0, db1, db2, db3, db4, db5, db6, db7);
+LiquidCrystal lcd(rs, en, db0, db1, db2, db3, db4, db5, db6, db7); // Inicializes the liquid crystal libriry
 void setup() {
-  lcd.begin(16, 2);
-  pinMode(menu_bt, INPUT_PULLUP);
+  lcd.begin(16, 2);                  // starts the liquid crysta display (LCD)
+  pinMode(menu_bt, INPUT_PULLUP);    // set pin 19 (menu_bt) as input and activates the pull up
   pinMode(enter_bt, INPUT_PULLUP);
   pinMode(up_bt, INPUT_PULLUP);
   pinMode(down_bt, INPUT_PULLUP);
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);              // set the pin 2 (led) as output
 }  //end setup
 
 
@@ -66,8 +66,8 @@ void setup() {
 // --- Main loop ---
 void loop() {
   if (!sub_menu && update_menu_flag) {  //if(not in submenu and update is needed)
-    lcd.clear();                   //clear display
-    switch (menu_number) {         //access main menu (prints it on LCD)
+    lcd.clear();                        //clear display
+    switch (menu_number) {              //access main menu (prints it on LCD)
       case 1:
         lcd.setCursor(0, 0);
         lcd.print(">Read Temp");
@@ -98,111 +98,45 @@ void loop() {
         lcd.setCursor(1, 1);
         lcd.print("Read Temp");
         break;
-    }  //end switch
-    update_menu_flag = 0;          //display is update no need to update it again unless a button is pressed
-  }    //end if(!sub_menu)
+    }                      //end switch
+    update_menu_flag = 0;  //display is update no need to update it again unless a button is pressed
+  }                        //end if(!sub_menu)
 
   read_bts();
 }  //end loop
 
 
 // =====================================================================================
-// --- Desenvolvimento das Funções ---
+// --- Function Development ---
 void read_bts() {
-  if (read_bt_menu())   return;
-  if (read_bt_enter ()) return;
-  if (read_bt_up())     return;
-  if (read_bt_down())   return;
-
-}  //end read_bts
-
-bool read_bt_menu() {
-  static bool menu_flag = 0;
-  static unsigned long menu_bounce = 0;
-  unsigned long current_time = millis();
-  //MENU BUTTON
-  if (!digitalRead(menu_bt) && (current_time - menu_bounce > 3)) { //if(button is pressed and it's been more than 3ms since last "press")
-    menu_bounce = current_time; 
-    menu_flag = 0x01;
-    return 1;
-  }  //end if(menu pressed)
-  if (digitalRead(menu_bt) && menu_flag && (current_time - menu_bounce > debounce_time)) { //if(button is reliesed and flag is activated and time since button press > debounce_time)
-    menu_bounce = current_time + 35;
-    menu_flag = 0x00;
+  if (read_bt_menu()) {
     if (sub_menu) {
-      update_menu_flag = 0x01;
       sub_menu = 0x00;
-    } //end if(submenu)
-    return 1;
-  }  //end if menu
-  return 0;
-}
-
-bool read_bt_enter() {
-  static bool enter_flag = 0;
-  static unsigned long enter_bounce = 0;
-  unsigned long current_time = millis();
-  //ENTER BUTTON
-  if (!digitalRead(enter_bt) && (current_time - enter_bounce > 3)) { //if(button is pressed and it's been more than 3ms since last "press")
-    enter_bounce = current_time; 
-    enter_flag = 0x01;
-    return 1;
-  }  //end if(enter pressed)
-  if (digitalRead(enter_bt) && enter_flag && (current_time - enter_bounce > debounce_time)) { //if(button is reliesed and flag is activated and time since button press > debounce_time)
-    enter_bounce = current_time + 35;
-    enter_flag = 0x00;
+      update_menu_flag = 0x01;
+    }  //end if(submenu)
+    return;
+  }
+  if (read_bt_enter()) {
     sub_menu = 0x01;
     menu_select();
-    return 1;
-  }  //end if enter
-  return 0;
-}
-
-bool read_bt_up() {
-  static bool up_flag = 0;
-  static unsigned long up_bounce = 0;
-  unsigned long current_time = millis();
-  //UP BUTTON
-  if (!digitalRead(up_bt) && (current_time - up_bounce > 3)) { //if(button is pressed and it's been more than 3ms since last "press")
-    up_bounce = current_time;
-    up_flag = 0x01;
-    return 1;
-  }  //end if(up pressed)
-  if (digitalRead(up_bt) && up_flag && (current_time - up_bounce > debounce_time)) { //if(button is reliesed and flag is activated and time since button press > debounce_time)
-    up_bounce = current_time + 35;
-    up_flag = 0x00;
+    return;
+  }
+  if (read_bt_up()) {
     update_menu_flag = 0x01;
     menu_number--;
     if (menu_number < 1) menu_number = MENU_MAX;
-    return 1;
-  }  //end if up
-  return 0;
-}
-
-bool read_bt_down() {
-  static bool down_flag = 0;
-  static unsigned long down_bounce = 0;
-  unsigned long current_time = millis();
-  //DOWN BUTTON
-  if (!digitalRead(down_bt) && (current_time - down_bounce > 3)) { //if(button is pressed and it's been more than 3ms since last "press")
-    down_bounce = current_time;
-    down_flag = 0x01;
-    return 1;
-  }  //end if(down pressed)
-  if (digitalRead(down_bt) && down_flag && (current_time - down_bounce > debounce_time)) { //if(button is reliesed and flag is activated and time since button press > debounce_time)
-    down_bounce = current_time + 35;
-    down_flag = 0x00;
+    return;
+  }
+  if (read_bt_down()) {
     update_menu_flag = 0x01;
     menu_number += 1;
     if (menu_number > MENU_MAX) menu_number = 1;
-    return 1;
-  }  //end if down
-  return 0;
-}
-
+    return;
+  }
+}  //end read_bts
 
 void menu_select() {
-  lcd.clear(); 
+  lcd.clear();
   switch (menu_number) {
     case 1:
       lcd.setCursor(0, 0);
@@ -232,14 +166,13 @@ void menu_select() {
       digitalWrite(led, LOW);
       break;
     case 5:
-      PORTD ^= (1<<led);
-      if (PIND & (1<<led)) {
+      PORTD ^= (1 << led);
+      if (PIND & (1 << led)) {
         lcd.setCursor(0, 0);
         lcd.print(">LED switch");
         lcd.setCursor(1, 1);
         lcd.print(">LED on");
-      }
-      else {
+      } else {
         lcd.setCursor(0, 0);
         lcd.print(">LED switch");
         lcd.setCursor(1, 1);
@@ -249,12 +182,82 @@ void menu_select() {
   }  //end switch
 }  //end menu_select
 
-
 float read_temperature() {
   float temperature = analogRead(0);
   temperature = temperature * 0.48828125 - 50;
   return temperature;
-} //end read_temperature
+}  //end read_temperature
+
+
+
+bool read_bt_menu() {
+  static bool menu_flag = 0;
+  static unsigned long menu_bounce = 0;
+  unsigned long current_time = millis();
+  //MENU BUTTON
+  if (!digitalRead(menu_bt) && (current_time - menu_bounce > 3)) {  //if(button is pressed and it's been more than 3ms since last "press")
+    menu_bounce = current_time;
+    menu_flag = 0x01;
+    return 0;
+  } //end if(menu pressed)
+  if (digitalRead(menu_bt) && menu_flag && (current_time - menu_bounce > debounce_time)) {  //if(button is reliesed and flag is activated and time since button press > debounce_time)
+    menu_bounce = current_time + 35;
+    menu_flag = 0x00;
+    return 1;
+  }  //end if(menu released)
+  return 0;
+}
+bool read_bt_enter() {
+  static bool enter_flag = 0;
+  static unsigned long enter_bounce = 0;
+  unsigned long current_time = millis();
+  //ENTER BUTTON
+  if (!digitalRead(enter_bt) && (current_time - enter_bounce > 3)) {  //if(button is pressed and it's been more than 3ms since last "press")
+    enter_bounce = current_time;
+    enter_flag = 0x01;
+    return 0;
+  }                                                                                            //end if(enter pressed)
+  if (digitalRead(enter_bt) && enter_flag && (current_time - enter_bounce > debounce_time)) {  //if(button is reliesed and flag is activated and time since button press > debounce_time)
+    enter_bounce = current_time + 35;
+    enter_flag = 0x00;
+    return 1;
+  }  //end if enter
+  return 0;
+}
+bool read_bt_up() {
+  static bool up_flag = 0;
+  static unsigned long up_bounce = 0;
+  unsigned long current_time = millis();
+  //UP BUTTON
+  if (!digitalRead(up_bt) && (current_time - up_bounce > 3) && !sub_menu) {  //if(button is pressed and it's been more than 3ms since last "press")
+    up_bounce = current_time;
+    up_flag = 0x01;
+    return 0;
+  }                                                                                   //end if(up pressed)
+  if (digitalRead(up_bt) && up_flag && (current_time - up_bounce > debounce_time)) {  //if(button is reliesed and flag is activated and time since button press > debounce_time)
+    up_bounce = current_time + 35;
+    up_flag = 0x00;
+    return 1;
+  }  //end if up
+  return 0;
+}
+bool read_bt_down() {
+  static bool down_flag = 0;
+  static unsigned long down_bounce = 0;
+  unsigned long current_time = millis();
+  //DOWN BUTTON
+  if (!digitalRead(down_bt) && (current_time - down_bounce > 3) && !sub_menu) {  //if(button is pressed and it's been more than 3ms since last "press")
+    down_bounce = current_time;
+    down_flag = 0x01;
+    return 0;
+  }                                                                                         //end if(down pressed)
+  if (digitalRead(down_bt) && down_flag && (current_time - down_bounce > debounce_time)) {  //if(button is reliesed and flag is activated and time since button press > debounce_time)
+    down_bounce = current_time + 35;
+    down_flag = 0x00;
+    return 1;
+  }  //end if down
+  return 0;
+}
 
 
 // =====================================================================================
